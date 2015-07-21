0630ae528746839509264c9e5b451b9a705b2dbf
Douglas Loyo <douglasloyo@gmail.com>
2015-07-21 15:33:11 -0500

    Adding alert directive.

---
+++ AngularWebApi/app/components/shared/alert/alertDirective.js
@@ -0,0 +1,13 @@
+﻿appDirectives.directive('myAlert', [function () {
+        return {
+            restrict: 'E',
+            //controller: 'alertController',
+            templateUrl: 'app/components/shared/alert/alertTemplate.html',
+            scope: {
+                viewModel: '=viewModel',
+                successMessage: '@',
+                errorMessage: '@',
+            },
+        }
+    }
+]);
